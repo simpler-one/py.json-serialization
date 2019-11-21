7b1f09e93c5bec3e1e5f96c1ef834ce65c26a10e
work-work-komei <40133262+work-work-komei@users.noreply.github.com>
2019-11-21 18:55:41 +0900

    Update info.py

--- info.py
+++ info.py
@@ -1,6 +1,16 @@
 class ClassInfo:
-    pass
+    def __init__(self, creator):
+        self.creator = creator
 
 
 class PropInfo:
-    pass
+    def __init__(self, json_key, mandatory, recursive, converter, setter)
+        self.json_key = json_key
+        self.mandatory = mandatory
+        self.recursive = recursive
+        self.converter = converter
+        self.setter = setter
+
+    def on_complete(cls, prop):
+        if self.json_key is None:
+            self.json_key = prop
