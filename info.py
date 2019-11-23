4e83bb4c1a7fb4622b4c0bb21d023a049266470e
work-work-komei <40133262+work-work-komei@users.noreply.github.com>
2019-11-23 14:36:45 +0900

    Update info.py

--- info.py
+++ info.py
@@ -1,16 +1,31 @@
+from meta_info import 
+
+
 class ClassInfo:
     def __init__(self, creator):
         self.creator = creator
 
 
-class PropInfo:
-    def __init__(self, json_key, mandatory, recursive, converter, setter)
+class PropInfo():
+    @classmethod
+    def _get_getter(cls, prop_name):
+        if prop_name.startswith("*"):
+            return lambda obj: obj
+        else:
+            return lambda obj: obj[prop_name]
+
+    def __init__(self, json_key, mandatory, recursive, converter, setter):
+        self.prop_name = ""
         self.json_key = json_key
         self.mandatory = mandatory
         self.recursive = recursive
         self.converter = converter
-        self.setter = setter
+        self.setter_name = ""
+        self._setter_pattern = setter
 
-    def on_complete(cls, prop):
+    def on_decorate(cls, prop):
+        self.prop_name = prop
         if self.json_key is None:
             self.json_key = prop
+
+        self.setter_name = self._setter_pattern.replace("$", prop)
