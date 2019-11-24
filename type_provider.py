7a5feba5fce75afaabbba75f3e2506442094a174
work-work-komei <40133262+work-work-komei@users.noreply.github.com>
2019-11-24 12:05:11 +0900

    Update type_provider.py

--- type_provider.py
+++ type_provider.py
@@ -3,7 +3,7 @@ from abc import abc, abstractmethod
 
 class TypeProvider(ABC):
     @abstractmethod
-    def get_type():
+    def get_type(self, self_cls, json_obj):
         raise NotImplementedError()
 
 
@@ -11,11 +11,16 @@ class SimpleType(TypeProvider):
     def __init__(self, obj_type):
         self.obj_type = obj_type
 
-    def get_type():
+    def get_type(self, self_cls, json_obj):
         return self.obj_type
 
 
-class WrappingType(TypeProvider, ABC):
+class SelfType(TypeProvider):
+    def get_type(self, self_cls, json_obj):
+        return self_cls
+
+
+class ConditionalType(TypeProvider):
     @classmethod
     def by_type():
         pass
