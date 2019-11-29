799f408604698668fee6461b45439a822e272859
work-work-komei <40133262+work-work-komei@users.noreply.github.com>
2019-11-29 12:59:03 +0900

    Update deserialization.py

--- deserialization.py
+++ deserialization.py
@@ -8,10 +8,24 @@ T = TypeVar("T")
 def parse(text, cls, option):
     return _from_json_obj(json.parse(text), cls, option, cls.__name__)
 
-def from_json_obj(obj, cls, option):
-    return _from_json_obj(obj, cls, option, cls.__name__)
+def from_json_obj(json_obj, cls, option):
+    return _from_json_obj(json_obj, cls, option, cls.__name__)
 
-def _from_json_obj(obj, cls, option, path):
+def _from_json_obj(src, cls, option, path):
     store = HELPER.get_store(cls)
     if store is None:
         raise ValueError("cls must be decorated by json_class")
+
+    # if store.cls.expected_types is not None and type(src) not in store.cls.expected_types:
+    #     raise ValueError(f"Unexpected JSON object expected: {}, actual: {}")
+
+    dst = store.cls.creator()
+
+    for key, member in store.members.items():
+        json_key = member.json_key
+        item = member.getter(src)
+        if item is None:
+            if member.mamdarory:
+                raise VallueError(f"Property is mandatory but null or not found: {path}")
+            else:
+                continue
