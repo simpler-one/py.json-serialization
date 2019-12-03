285a3dc4ef28e71f468e6ea37951cc75e04d97c1
work-work-komei <40133262+work-work-komei@users.noreply.github.com>
2019-12-03 18:14:33 +0900

    Update deserialization.py

--- src/deserialization.py
+++ src/deserialization.py
@@ -9,13 +9,13 @@ NoneType = type(None)
 CONTAINER_TYPES = {list, dict}
 
 
-def parse(text, cls, option):
+def parse(text, cls, option=None):
     if not _validate_root_class(cls):
         raise ValueError()
     return _from_json_obj(json.loads(text), cls, option, cls.__name__)
 
 
-def from_json_obj(json_obj, cls, option):
+def from_json_obj(json_obj, cls, option=None):
     if not _validate_root_class(cls):
         raise ValueError()
     return _from_json_obj(json_obj, cls, option, cls.__name__)
@@ -29,7 +29,7 @@ def _from_json_obj(source, cls, option, path):
     if store is None:
         raise ValueError("cls must be decorated by json_class")
 
-    if store.cls.validate_type(type(source)):
+    if not store.cls.validate_type(type(source)):
         raise ValueError(f"Unexpected JSON object expected: {store.cls.expected_types}, actual: {type(source)}")
 
     dst = store.cls.creator()
@@ -38,8 +38,8 @@ def _from_json_obj(source, cls, option, path):
         cur_path = path + member.prop_selector.path
         src = member.prop_selector.get_from(source)
         if src is None:
-            if member.mamdarory:
-                raise ValueError(f"Property is mandatory but null or not found: {path}")
+            if member.mandatory:
+                raise ValueError(f"Property is mandatory but null or not found: {cur_path}")
             else:
                 continue
 
@@ -48,17 +48,18 @@ def _from_json_obj(source, cls, option, path):
             value = _expand(src, cls, member, option, cur_path)
 
         setattr(dst, member.setter_name, value)
+    return dst
 
 
 def _expand(source, cls, member, opt, path):
     get_type = member.type_provider.get_type
-    if isinstance(member, ListType):
+    if isinstance(member.type_provider, ListType):
         return [
             _from_json_obj(src, get_type(cls, src), opt, f"{path}[{i}]")
             for i, src in enumerate(source)
             if get_type(cls, src) is not NoneType
         ]
-    elif isinstance(member, MapType):
+    elif isinstance(member.type_provider, MapType):
         return {
             key: _from_json_obj(src, get_type(cls, src), opt, f"{path}.{key}")
             for key, src in source.items()
